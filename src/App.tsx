8515b09432120910dfe5d0d97e39f45370798324
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-10-30 15:23:29 +0300

    default setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Routes } from 'react-router-dom';
 
-function App() {
+const App = () => {
+  const [tabs, setTabs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const load = () => {
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        {}
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+
+export const Header = () => {
+  return (
+    <header className='header'>
+      <nav className='header__nav nav'>
+        <ul className='nav__list'>
+          <li className='nav__item'>
+            <a className='nav__link'></a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
